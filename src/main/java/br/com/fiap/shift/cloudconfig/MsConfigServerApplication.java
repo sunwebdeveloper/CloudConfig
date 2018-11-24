9d5882fad75b6edd709f9f0426f7762c75df67f0
Usuário Convidado <Guest@PA06MICRO15.local>
2018-11-24 12:30:38 -0200

    Configuracao basica inicial

---
+++ src/main/java/br/com/fiap/shift/cloudconfig/MsConfigServerApplication.java
@@ -0,0 +1,16 @@
+package br.com.fiap.shift.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class MsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigServerApplication.class, args);
+	}
+}
